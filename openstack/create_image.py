c2165f42071018eeab0f9cc45680bc19ccb45a08
spisarski <s.pisarski@cablelabs.com>
2015-10-14 13:14:33 -0600

    Completed logic to create OpenStack images using the glance and keystone clients as well as creating the necessary unit tests.

--- openstack/create_image.py
+++ openstack/create_image.py
@@ -1,5 +1,9 @@
-import logging, sdn_utils, glanceclient
-import os
+import logging, sdn_utils, os
+from glanceclient import Client
+from keystoneclient.auth.identity import v2 as identity
+from keystoneclient import session
+import keystoneclient.v2_0.client as ksclient
+import shutil
 
 logger = logging.getLogger('create_image')
 
@@ -8,15 +12,22 @@ class CreateImage():
     Class responsible for creating an image in OpenStack
     """
 
-    def __init__(self, name, url, format, destPath):
+    def __init__(self, username, password, osAuthUrl, tenantName, format, imageUrl, imageName, destPath):
         """Constructor"""
-        self.name = name
-        self.url = url
+        self.username = username
+        self.password = password
+        self.osAuthUrl = osAuthUrl
+        self.tenantName = tenantName
         self.format = format
+        self.imageUrl = imageUrl
+        self.imageName = imageName
         self.destPath = destPath
 
-        fileName = url.rsplit('/')[-1]
-        self.imageFilePath = destPath + fileName
+        fileName = imageUrl.rsplit('/')[-1]
+        self.imageFilePath = destPath + '/' + fileName
+
+        self.image = None
+        self.imageFile = None
 
     def create(self):
         """
@@ -24,34 +35,64 @@ class CreateImage():
         :return: The OpenStack Image object
         """
         self.imageFile = self.__getImageFile()
-        glance = glanceclient.Client('1')
-        self.image = glance.images.create(name=self.name)
-        self.image.update(data=self.imageFile)
+        glance = self.glanceClient()
+        self.image = glance.images.create(name=self.imageName, disk_format=self.format,
+                                          container_format="bare", data=self.imageFile.name)
+        glance.images.upload(self.image.id, open(self.imageFile.name, 'rb'))
         return self.image
 
-    def cleanEnv(self):
+    def clean(self):
         """
         Cleanse environment of all artifacts
         :return: void
         """
-        self.image.delete()
-        os.remove(self.imageFile.name)
+        if self.image:
+            glance = self.glanceClient()
+            glance.images.delete(self.image.id)
+
+        if self.imageFile:
+            shutil.rmtree(self.destPath)
+
+    def glanceClient(self):
+        """
+        Creates and returns a glance client object
+        :return: the glance client
+        """
+        creds = self.__get_keystone_creds()
+        keystone = ksclient.Client(**creds)
+        glance_endpoint = keystone.service_catalog.url_for(service_type='image', endpoint_type='publicURL')
+        auth = identity.Password(auth_url=self.osAuthUrl, username=self.username, password=self.password,
+                                 tenant_name=self.tenantName)
+        sess = session.Session(auth=auth)
+        token = auth.get_token(sess)
+        return Client('2', endpoint=glance_endpoint, token=token)
 
     def __getImageFile(self):
         """
         Returns True if the image file has already been downloaded
         :return: the image file object
         """
-        if not sdn_utils.fileExists(self.imageFilePath):
+        if sdn_utils.fileExists(self.imageFilePath):
             return open(self.imageFilePath, 'r')
         else:
+            if not os.path.exists(self.destPath):
+                os.makedirs(self.destPath)
             return self.__downloadImageFile()
+
+    def __get_keystone_creds(self):
+        """
+        Creates the object to hold the credentials for Keystone
+        :return: the credentials
+        """
+        d = {'username': self.username, 'password': self.password, 'auth_url': self.osAuthUrl,
+             'tenant_name': self.tenantName}
+        return d
 
     def __downloadImageFile(self):
         """
         Downloads the image file
         :return: the image file object
         """
-        if(not sdn_utils.fileExists(self.imageFilePath)):
-            return sdn_utils.download(self.url, self.destPath)
+        if not sdn_utils.fileExists(self.imageFilePath):
+            return sdn_utils.download(self.imageUrl, self.destPath)
 
--- openstack/tests/create_image_tests.py
+++ openstack/tests/create_image_tests.py
@@ -1,16 +1,173 @@
 import unittest
-import openstack.create_image as createImage
+import os
 
-class CreateImageTests(unittest.TestCase):
+import sdn_utils
+import openstack.create_image as create_image
+
+# This is currently pointing to the CL OPNFV Lab 2 environment and these tests will break should there not be network
+# connectivity to this location.
+osAuthUrl = 'http://10.197.123.37:5000/v2.0'
+
+username = 'admin'
+password = 'octopus'
+tenantName = 'admin'
+imageFormat = 'qcow2'
+imageUrl = 'http://download.cirros-cloud.net/0.3.4/cirros-0.3.4-x86_64-disk.img'
+imageName = 'test-image'
+destPath = '/tmp/create_image_tests'
+
+
+class CreateImageSuccessTests(unittest.TestCase):
+    """
+    Test for the CreateImage class defined in create_image.py
+    """
 
     def setUp(self):
-        print 'Setting up'
-        self.createImage = createImage.CreateImage('testImage',
-                                                   'http://download.cirros-cloud.net/0.3.4/cirros-0.3.4-x86_64-disk.img',
-                                                   'qcow2', '/bar')
+        """
+        Instantiates the CreateImage object that is responsible for downloading and creating an OS image file
+        within OpenStack
+        """
+        self.createImage = create_image.CreateImage(username, password, osAuthUrl, tenantName, imageFormat, imageUrl,
+                                                   imageName, destPath)
 
-    def test1(self):
+    def tearDown(self):
+        """
+        Cleans the image and downloaded image file
+        """
+        self.createImage.clean()
+
+    def testCreateImageClean(self):
+        """
+        Tests the creation of an OpenStack image when the download directory does not exist.
+        """
+        # Create Image
         self.createImage.create()
+        glance = self.createImage.glanceClient()
+        images = glance.images.list()
 
-    def test2(self):
-        print 'Test 2'
+        # Validate
+        found = False
+        for image in images:
+            if image.name == self.createImage.imageName:
+                found = True
+
+        self.assertEquals(True, found)
+
+    def testCreateImageWithExistingDownloadDirectory(self):
+        """
+        Tests the creation of an OpenStack image when the image file directory exists but not the image file.
+        """
+        # Create download directory
+        os.makedirs(destPath)
+
+        # Create Image
+        self.createImage.create()
+        glance = self.createImage.glanceClient()
+        images = glance.images.list()
+
+        # Validate
+        found = False
+        for image in images:
+            if image.name == self.createImage.imageName:
+                found = True
+
+        self.assertEquals(True, found)
+
+    def testCreateImageWithExistingImageFile(self):
+        """
+        Tests the creation of an OpenStack image when the image file exists.
+        """
+        # Create download directory
+        os.makedirs(destPath)
+
+        # Download image file
+        sdn_utils.download(imageUrl, destPath)
+
+        # Create Image
+        self.createImage.create()
+        glance = self.createImage.glanceClient()
+        images = glance.images.list()
+
+        # Validate
+        found = False
+        for image in images:
+            if image.name == self.createImage.imageName:
+                found = True
+
+        self.assertEquals(True, found)
+
+
+class CreateImageNegativeTests(unittest.TestCase):
+    """
+    Negative test cases for the CreateImage class
+    """
+
+    def setUp(self):
+        self.createImage = None
+
+    def tearDown(self):
+        if self.createImage is not None:
+            self.createImage.clean()
+
+    def testInvalidDirectory(self):
+        """
+        Expect an exception when the download destination path cannot be created
+        """
+        self.createImage = create_image.CreateImage(username, password, osAuthUrl, tenantName, imageFormat, imageUrl,
+                                                   imageName, '/foo')
+        with self.assertRaises(Exception):
+            self.createImage.create()
+
+    def testNoneImageName(self):
+        """
+        Expect an exception when the image name is None
+        """
+        self.createImage = create_image.CreateImage(username, password, osAuthUrl, tenantName, imageFormat, imageUrl,
+                                                   None, destPath)
+        with self.assertRaises(Exception):
+            self.createImage.create()
+
+    def testBadImageUrl(self):
+        """
+        Expect an exception when the image download url is bad
+        """
+        self.createImage = create_image.CreateImage(username, password, osAuthUrl, tenantName, imageFormat,
+                                                   'http://bad.url.com/bad.iso', imageName, destPath)
+        with self.assertRaises(Exception):
+            self.createImage.create()
+
+    def testNoneTenantName(self):
+        """
+        Expect an exception when the tenant name is None
+        """
+        self.createImage = create_image.CreateImage(username, password, osAuthUrl, None, imageFormat,
+                                                   imageUrl, imageName, destPath)
+        with self.assertRaises(Exception):
+            self.createImage.create()
+
+    def testNoneAuthUrl(self):
+        """
+        Expect an exception when the tenant name is None
+        """
+        self.createImage = create_image.CreateImage(username, password, None, tenantName, imageFormat,
+                                                   imageUrl, imageName, destPath)
+        with self.assertRaises(Exception):
+            self.createImage.create()
+
+    def testNonePassword(self):
+        """
+        Expect an exception when the tenant name is None
+        """
+        self.createImage = create_image.CreateImage(username, None, osAuthUrl, tenantName, imageFormat,
+                                                   imageUrl, imageName, destPath)
+        with self.assertRaises(Exception):
+            self.createImage.create()
+
+    def testNoneUser(self):
+        """
+        Expect an exception when the tenant name is None
+        """
+        self.createImage = create_image.CreateImage(None, password, osAuthUrl, tenantName, imageFormat,
+                                                   imageUrl, imageName, destPath)
+        with self.assertRaises(Exception):
+            self.createImage.create()
