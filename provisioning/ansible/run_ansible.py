d9fa631924ec876659c9a9f94e8e106e16bcf72a
spisarski <s.pisarski@cablelabs.com>
2016-02-04 10:18:17 -0700

    Changed ansible ping prototype code to accept private_keys instead of using password authentication.

--- provisioning/ansible/run_ansible.py
+++ provisioning/ansible/run_ansible.py
@@ -4,7 +4,6 @@
 # Equivalent to cmdline call
 # sudo ansible -i provisioning/ansible/conf/inventory -m ping -u root all -k
 # TODO - Make configurable. This cut was checked in simply to demonstrate how it is done.
-
 import logging
 
 from ansible.runner import Runner
@@ -17,10 +16,18 @@ def main():
     logging.basicConfig(level=logging.DEBUG)
     logger.info('Start Run Ansible')
 
-    hosts = ['192.168.253.142']
+    hosts = ['10.197.123.211']
     inventory = Inventory(host_list=hosts)
 
-    runner = Runner(module_name='ping', inventory=inventory, pattern='all', remote_user='root', remote_pass='cable123')
+    """
+    NOTE: This test and any others that call ansible will most likely fail unless you do one of
+    two things:
+    1. Have a ~/.ansible.cfg (or alternate means) to set host_key_checking = False
+    2. Set the following environment variable in your executing shell: ANSIBLE_HOST_KEY_CHECKING=False
+    Should this not be performed, the creation of the host ssh key will cause your ansible calls to fail.
+    """
+    runner = Runner(module_name='ping', inventory=inventory, pattern='all', remote_user='centos',
+                    private_key_file='/tmp/testKP')
     data = runner.run()
     print data
 
