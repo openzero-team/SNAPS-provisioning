cbe07220671f65ecd47c1c53dedaa9c7064e2bde
spisarski <s.pisarski@cablelabs.com>
2016-02-05 16:31:20 -0700

    Ansible scripts for deploying the UNI Manager services and OpenVSwitch.

---
+++ provisioning/ansible/unimgr/run_playbook.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+#
+# This script is responsible for executing the Ansible playbook java7.yaml
+# Equivalent to cmdline call
+# ansible-playbook -i conf/inventory ansible/playbooks/java7.yaml -u {sudo user} -U root --private-key {path}
+# TODO - Make configurable. This cut was checked in simply to demonstrate how it is done.
+
+import logging
+
+from ansible.playbook import PlayBook
+from ansible.inventory import Inventory
+from ansible.callbacks import AggregateStats
+from ansible.callbacks import PlaybookRunnerCallbacks
+from ansible.callbacks import PlaybookCallbacks
+from ansible import utils
+
+logger = logging.getLogger('Unimgr service')
+
+
+def main():
+    logging.basicConfig(level=logging.DEBUG)
+    logger.info('Start Run Ansible')
+
+    hosts = ['10.197.123.205']
+    inventory = Inventory(host_list=hosts)
+
+    stats = AggregateStats()
+    run_cb = PlaybookRunnerCallbacks(stats, verbose=utils.VERBOSITY)
+    pb_cb = PlaybookCallbacks(verbose=utils.VERBOSITY)
+
+    runner = PlayBook(playbook='provisioning/ansible/unimgr/main.yml', inventory=inventory, remote_user='centos',
+                      sudo_user='root', private_key_file='/tmp/unimgr', sudo=True, callbacks=pb_cb,
+                      runner_callbacks=run_cb, stats=stats)
+    data = runner.run()
+    print data
+
+
+if __name__ == '__main__':
+    main()
--- provisioning/ansible/unimgr/scripts/startTomcatWithGui.sh
+++ provisioning/ansible/unimgr/scripts/startTomcatWithGui.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 # Script 5 - Copy war files to tomcat and start Tomcat
 
-home='/home/vbcpe'  # change this to your home directory path
 CATALINA_HOME='/usr/local/apache-tomcat/apache-tomcat-8.0.24'
-echo "Copying war files" >>  $home/mdsal_ansible/install.log
-echo $CATALINA_HOME >> $home/mdsal_ansible/install.log
-cd $home/mdsal_ansible/lsoapi
+echo "Copying war files" >>  ~/mdsal_ansible/install.log
+echo $CATALINA_HOME >> ~/mdsal_ansible/install.log
+cd ~/mdsal_ansible/lsoapi
 sudo cp ./cos/cosmgr/target/cosmgr.war $CATALINA_HOME/webapps/.
 sudo cp ./evc/evcmgr/target/evcmgr.war $CATALINA_HOME/webapps/.
 sudo cp ./svc/svcmgr/target/svcmgr.war $CATALINA_HOME/webapps/.
 sudo $CATALINA_HOME/bin/shutdown.sh &
 nohup sudo $CATALINA_HOME/bin/startup.sh &
 
-echo "Tomcat started" >>  $home/mdsal_ansible/install.log
-cd $home/mdsal_ansible/lsoapi/demo-ui
-sudo npm install >>  $home/mdsal_ansible/lsoapi.log
+echo "Tomcat started" >>  ~/mdsal_ansible/install.log
+cd ~/mdsal_ansible/lsoapi/demo-ui
+sudo npm install >>  ~/mdsal_ansible/lsoapi.log
 
