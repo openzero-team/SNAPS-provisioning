314f601bfd917b0b5547067792f1cf3f011ef944
spisarski <s.pisarski@cablelabs.com>
2016-05-19 16:19:35 -0600

    Added copyright and license.

--- provisioning/ansible/unimgr/scripts/runUnimgr.sh
+++ provisioning/ansible/unimgr/scripts/runUnimgr.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
-# Script 6 - Make changes for the IP address and MAC address, build lsoapi and unimgr code and start the UI and karaf 
+# Copyright (c) 2016 Cable Television Laboratories, Inc. ("CableLabs")
+#                    and others.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Script 6 - Make changes for the IP address and MAC address, build lsoapi and unimgr code and start the UI and karaf
 
 cd $HOME/mdsal_ansible/lsoapi/demo-ui
 bower --allow-root install >>  $HOME/mdsal_ansible/lsoapi.log
--- setup.py
+++ setup.py
@@ -1,19 +1,36 @@
+# Copyright (c) 2016 Cable Television Laboratories, Inc. ("CableLabs")
+#                    and others.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+__author__ = 'spisarski'
+
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
 config = {
-    'description': 'My Project',
-    'author': 'My Name',
+    'description': 'Virtual Environment Creation Framework',
+    'author': 'Steve Pisarski',
     'url': 'URL to get it at.',
     'download_url': 'Where to download it.',
-    'author_email': 'My email.',
+    'author_email': 's.pisarski@cablelabs.com',
     'version': '0.1',
-    'install_requires': ['nose'],
+    'install_requires': ['ansible', 'Crypto', 'python-novaclient', 'python-glanceclient', 'python-neutronclient',
+                         'python-keystoneclient', 'scp', 'PyYAML'],
     'packages': ['NAME'],
     'scripts': [],
-    'name': 'projectname'
+    'name': 'provisioning'
 }
 
-setup(**config)
+setup(**config, requires=['ansible', 'Crypto', 'python-keystoneclient', 'scp', 'PyYAML'])
