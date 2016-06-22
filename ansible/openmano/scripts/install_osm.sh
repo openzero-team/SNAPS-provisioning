739de83227ba674742e813c654f42a9035ea4d74
spisarski <s.pisarski@cablelabs.com>
2016-06-22 09:26:15 -0600

    Fixed the installation of OpenMano.

--- ansible/openmano/scripts/install_osm.sh
+++ ansible/openmano/scripts/install_osm.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-wget -O install-openmano.sh "https://osm.etsi.org/gitweb/?p=osm/openmano.git;a=blob_plain;f=scripts/install-openmano.sh"
+wget -O /tmp/install-openmano.sh "https://osm.etsi.org/gitweb/?p=osm/openmano.git;a=blob_plain;f=scripts/install-openmano.sh"
 
-chmod +x install-openmano.sh
+chmod +x /tmp/install-openmano.sh
 
-./install-openmano.sh
+/tmp/install-openmano.sh -u root -p cable123 -q | tee /tmp/install-openmano.out
