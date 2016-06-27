a88b84e39f5518152edd9fa7960c0e934c315936
spisarski <s.pisarski@cablelabs.com>
2016-06-27 15:12:14 -0600

    Completed installation and starting of OpenBaton.

--- ansible/openbaton/scripts/install_openbaton.sh
+++ ansible/openbaton/scripts/install_openbaton.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-bash <(curl -fsSkL http://get.openbaton.org/bootstrap)
+echo '{{ rabbit_broker_ip }}\n{{ rabbit_mgmt_port }}\n{{ rabbit_enable_security }}\n{{ rabbit_enable_https }}\n' | \
+bash <(curl -fsSkL http://get.openbaton.org/bootstrap) | \
+tee /tmp/install_openbaton.out
+
+cd /opt/openbaton/nfvo/
+
+./openbaton.sh start
